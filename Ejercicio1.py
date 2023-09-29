c0089ab432e72fec4fd5b1b2f03aa6de03af792a
GonzaloSanchezCalvente <gonzalosanchez910.alu@stemgranada.com>
2023-09-29 08:49:34 +0200

    Soluciones

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -4,3 +4,9 @@ f"3. Este Pokémon es un pájaro grande de tipo volador. Es conocido por su velo
 f"4. Es un Pokémon dragón que puede lanzar llamas ardientes de su boca. Es el último eslabón de una evolución de tres etapas.\n"
 f"5. Este Pokémon es un pequeño roedor eléctrico con mejillas rojas y amarillas. Es la mascota más icónica de la franquicia Pokémon.\n"
 )
+
+print(f"1. La serpiente venenosa con un patrón de colores brillantes es Ekans.\n"
+      f"2. El Pokémon con una concha dura y cañones de agua en su espalda es Squirtle.\n"
+      f"3. El pájaro grande de tipo volador es Pidgeot.\n"
+      f"4. El dragón que lanza llamas ardientes de su boca es Charizard.\n"
+      f"5. El pequeño roedor eléctrico con mejillas rojas y amarillas es Pikachu.\n")
